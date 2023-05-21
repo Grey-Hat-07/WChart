8957d7cffe9cf5ff312cb8a78565e34e7f193102
grey-hat-07 <subhamaynath2002@gmail.com>
2023-05-21 18:26:39 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { weather } from './weather'
+import React, { useState, useEffect } from 'react'
+import { Line, Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement
+} from 'chart.js';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement
+);
+
+
 
 function App() {
+
+  const [location, setLocation] = useState('Kolkata')
+  const [selectedCityData, setSelectedCityData] = useState([]);
+  const [labels, setLabels] = useState([]);
+  const [temperatures, setTemperatures] = useState([]);
+  const [windSpeed, setWindSpeed] = useState([]);
+  const searchWeatherData = () => {
+    const selectedCity = weather.find(cityData => cityData.city === location);
+    if (selectedCity) {
+
+      const cityWeatherData = selectedCity.data;
+      const label = cityWeatherData.map(data => data.date)
+      const temperature = cityWeatherData.map(data => data.temperature)
+      const wind = cityWeatherData.map(data => data.windSpeed)
+      setSelectedCityData(cityWeatherData);
+      setLabels(label);
+      setTemperatures(temperature);
+      setWindSpeed(wind);
+    } else {
+      
+      setSelectedCityData([]);
+    }
+  };
+  // searchWeatherData()
+  useEffect(() => {
+    searchWeatherData();
+  }, []);
+  // const weather = async () => {
+  //   const res = await fetch(`${url}/api`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({ product: data })
+  //   })
+  // }
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Temperature',
+        data: temperatures,
+        fill: true,
+        borderColor: 'rgb(10, 192, 192)',
+
+        tension: 0.3,
+      },
+    ],
+  };
+  const windSpeedData = {
+    labels,
+    datasets: [
+      {
+        label: 'Wind Speed',
+        data: windSpeed,
+        fill: false,
+        borderColor: 'rgb(75, 192, 100)',
+        tension: 0.1,
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <form className="justify-center flex mt-5">
+        <div className="flex w-full items-center space-x-2 md:w-1/3">
+          <input
+            className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <button
+            type="submit"
+            class="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            onClick={(e) => {
+              e.preventDefault();
+              searchWeatherData();
+            }}
+          >
+            Search
+          </button>
+
+        </div>
+      </form>
+      <div className="grid grid-cols-2 p-5">
+        <div>
+          {chartData && <Line data={chartData} />}
+        </div>
+        <div>
+          {windSpeedData && <Bar data={windSpeedData} />}
+        </div>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/weather.js
@@ -0,0 +1,55 @@
+// import React from 'react'
+
+ export const weather=[{
+    city: "Kolkata",
+    data: [
+      { date: "2023-05-13", temperature: 30, precipitation: 0.2, windSpeed: 10, humidity: 80 },
+      { date: "2023-05-14", temperature: 32, precipitation: 0.0, windSpeed: 12, humidity: 75 },
+      { date: "2023-05-15", temperature: 34, precipitation: 0.1, windSpeed: 8, humidity: 70 },
+      { date: "2023-05-16", temperature: 31, precipitation: 0.0, windSpeed: 10, humidity: 82 },
+      { date: "2023-05-17", temperature: 33, precipitation: 0.3, windSpeed: 15, humidity: 78 },
+      { date: "2023-05-18", temperature: 29, precipitation: 0.1, windSpeed: 11, humidity: 85 },
+      { date: "2023-05-19", temperature: 28, precipitation: 0.0, windSpeed: 9, humidity: 88 },
+    ],
+  },
+  {
+    city: "Mumbai",
+    data: [
+      { date: "2023-05-13", temperature: 35, precipitation: 0.0, windSpeed: 14, humidity: 72 },
+      { date: "2023-05-14", temperature: 33, precipitation: 0.0, windSpeed: 16, humidity: 68 },
+      { date: "2023-05-15", temperature: 32, precipitation: 0.2, windSpeed: 13, humidity: 70 },
+      { date: "2023-05-16", temperature: 34, precipitation: 0.1, windSpeed: 15, humidity: 75 },
+      { date: "2023-05-17", temperature: 31, precipitation: 0.0, windSpeed: 12, humidity: 78 },
+      { date: "2023-05-18", temperature: 30, precipitation: 0.0, windSpeed: 10, humidity: 80 },
+      { date: "2023-05-19", temperature: 29, precipitation: 0.1, windSpeed: 11, humidity: 82 },
+    ],
+  },
+  {
+    city: "Chennai",
+    data: [
+      { date: "2023-05-13", temperature: 36, precipitation: 0.0, windSpeed: 12, humidity: 75 },
+      { date: "2023-05-14", temperature: 37, precipitation: 0.1, windSpeed: 10, humidity: 70 },
+      { date: "2023-05-15", temperature: 35, precipitation: 0.0, windSpeed: 8, humidity: 68 },
+      { date: "2023-05-16", temperature: 33, precipitation: 0.0, windSpeed: 11, humidity: 72 },
+      { date: "2023-05-17", temperature: 32, precipitation: 0.2, windSpeed: 9, humidity: 78 },
+      { date: "2023-05-18", temperature: 31, precipitation: 0.0, windSpeed: 10, humidity: 80 },
+      { date: "2023-05-19", temperature: 30, precipitation: 0.1, windSpeed: 11, humidity: 82 },
+    ],
+  },
+  {
+    city: "Bengaluru",
+    data: [
+      { date: "2023-05-13", temperature: 30, precipitation: 0.0, windSpeed: 8, humidity: 70 },
+      { date: "2023-05-14", temperature: 32, precipitation: 0.1, windSpeed: 7, humidity: 68 },
+      { date: "2023-05-15", temperature: 31, precipitation: 0.0, windSpeed: 9, humidity: 72 },
+      { date: "2023-05-16", temperature: 29, precipitation: 0.0, windSpeed: 8, humidity: 75 },
+      { date: "2023-05-17", temperature: 28, precipitation: 0.2, windSpeed: 7, humidity: 78 },
+      { date: "2023-05-18", temperature: 27, precipitation: 0.0, windSpeed: 6, humidity: 80 },
+      { date: "2023-05-19", temperature: 28, precipitation: 0.1, windSpeed: 7, humidity: 82 },
+    ],
+  },
+
+]
+
+
+
